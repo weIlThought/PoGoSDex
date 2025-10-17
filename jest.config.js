10affb0dbfbb1e959e9d6782dd75f701b2b77bfe
weIlThought <rp.jamescruz@gmail.com>
2025-10-17 22:14:30 +0200

    Add dotenv dependency and create server package.json for backend setup
    - Added dotenv as a dependency in the main package.json for environment variable management.
    - Created a new server/package.json file for the backend, including necessary dependencies and scripts for development and production.

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   testEnvironment: "node",
+
+  rootDir: "./",
+
+  moduleDirectories: ["node_modules", "server", "public"],
+
+  moduleFileExtensions: ["js", "json", "ts"],
+  testPathIgnorePatterns: ["/node_modules/", "/public/"],
 };
